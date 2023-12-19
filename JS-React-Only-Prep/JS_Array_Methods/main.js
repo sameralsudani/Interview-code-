4374f5ca933d32daf77958a627171a2a846dfa24
sameralsudani <samerxxxalsudani@gmail.com>
2023-12-19 13:27:13 -0600

    Fix

--- JS-React-Only-Prep/JS_Array_Methods/main.js
+++ JS-React-Only-Prep/JS_Array_Methods/main.js
@@ -1,134 +1,139 @@
-const companies = [{
+const companies = [
+  {
     name: "Company One",
     category: "Finance",
     start: 1981,
-    end: 2004
+    end: 2004,
   },
   {
     name: "Company Two",
     category: "Retail",
     start: 1992,
-    end: 2008
+    end: 2008,
   },
   {
     name: "Company Three",
     category: "Auto",
     start: 1999,
-    end: 2007
+    end: 2007,
   },
   {
     name: "Company Four",
     category: "Retail",
     start: 1989,
-    end: 2010
+    end: 2010,
   },
   {
     name: "Company Five",
     category: "Technology",
     start: 2009,
-    end: 2014
+    end: 2014,
   },
   {
     name: "Company Six",
     category: "Finance",
     start: 1987,
-    end: 2010
+    end: 2010,
   },
   {
     name: "Company Seven",
     category: "Auto",
     start: 1986,
-    end: 1996
+    end: 1996,
   },
   {
     name: "Company Eight",
     category: "Technology",
     start: 2011,
-    end: 2016
+    end: 2016,
   },
   {
     name: "Company Nine",
     category: "Retail",
     start: 1981,
-    end: 1989
-  }
+    end: 1989,
+  },
 ];
 
 const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
 //////////////////////////
 // for loop
-for(let i = 0; i < companies.length; i++) {
+for (let i = 0; i < companies.length; i++) {
   console.log(companies[i]);
 }
 
 //////////////////////////
 // forEach loop
-companies.forEach(function(company) {
+companies.forEach(function (company) {
   console.log(company.name);
 });
 
 //////////////////////////
 // Get 21 and older
 let canDrink = [];
-for(let i = 0; i < ages.length; i++) {
-  if(ages[i] >= 21) {
+for (let i = 0; i < ages.length; i++) {
+  if (ages[i] >= 21) {
     canDrink.push(ages[i]);
   }
 }
-console.log(canDrink)
+console.log(canDrink);
 
 //Or Using filter
-const canDrink2 = ages.filter(function(age) {
-  if(age >= 21) {
+const canDrink2 = ages.filter(function (age) {
+  if (age >= 21) {
     return true;
   }
 });
 
 //Or using filter (2nd way)
-const canDrink3 = ages.filter(age => age >= 21);
-
+const canDrink3 = ages.filter((age) => age >= 21);
 
 //////////////////////////
 // Filter retail companies
-const retailCompanies = companies.filter(function(company) {
-  if(company.category === 'Retail') {
+const retailCompanies = companies.filter(function (company) {
+  if (company.category === "Retail") {
     return true;
   }
 });
 //Or Using filter
-const retailCompanies2 = companies.filter(company => company.category === 'Retail');
-
+const retailCompanies2 = companies.filter(
+  (company) => company.category === "Retail"
+);
 
 //////////////////////////
 // Get 80s companies
-const eightiesCompanies = companies.filter(company => (company.start >= 1980 && company.start < 1990));
-
+const eightiesCompanies = companies.filter(
+  (company) => company.start >= 1980 && company.start < 1990
+);
 
 //////////////////////////
 // Get companies that lasted 10 years or more
-const lastedTenYears = companies.filter(company => (company.end - company.start >= 10));
+const lastedTenYears = companies.filter(
+  (company) => company.end - company.start >= 10
+);
 
 //////////////////////////
 // Create array of company names
-const companyNames = companies.map(function(company) {
+const companyNames = companies.map(function (company) {
   return company.name;
 });
-console.log(companyNames)
+console.log(companyNames);
 
 //////////////////////////
 // Create array of `company name [start - end]`
-const testMap = companies.map(function(company) {
+const testMap = companies.map(function (company) {
   return `${company.name} [${company.start} - ${company.end}]`;
 });
 //Or
-const testMap2 = companies.map(company => `${company.name} [${company.start} - ${company.end}]`);
-
+const testMap2 = companies.map(
+  (company) => `${company.name} [${company.start} - ${company.end}]`
+);
 
 //////////////////////////
 // Sort companies by start year
-const sortedCompanies1  = companies.sort(function(c1, c2) {
-  if(c1.start > c2.start) {
+const sortedCompanies1 = companies.sort(function (c1, c2) {
+  if (c1.start > c2.start) {
     return 1;
   } else {
     return -1;
@@ -142,15 +147,14 @@ const sortedCompanies = companies.sort((a, b) => (a.start > b.start ? 1 : -1));
 const sortAges = ages.sort((a, b) => a - b);
 console.log(sortAges);
 
-
 //////////////////////////
 // find sum of age
 let ageSum1 = 0;
-for(let i = 0; i < ages.length; i++) {
+for (let i = 0; i < ages.length; i++) {
   ageSum += ages[i];
 }
 //Or using reduce
-const ageSum2 = ages.reduce(function(total, age) {
+const ageSum2 = ages.reduce(function (total, age) {
   return total + age;
 }, 0);
 //Or using reduce (2nd way)
@@ -158,23 +162,25 @@ const ageSum = ages.reduce((total, age) => total + age, 0);
 
 //////////////////////////
 // Get total years for all companies
-const totalYears1 = companies.reduce(function(total, company) {
+const totalYears1 = companies.reduce(function (total, company) {
   return total + (company.end - company.start);
 }, 0);
 
 // OR
-const totalYears2 = companies.reduce((total, company) => total + (company.end - company.start), 0);
+const totalYears2 = companies.reduce(
+  (total, company) => total + (company.end - company.start),
+  0
+);
 
 //////////////////////////
 // Combine all Methods
 const combined = ages
-  .map(age => age * 2)
-  .filter(age => age >= 40)
+  .map((age) => age * 2)
+  .filter((age) => age >= 40)
   .sort((a, b) => a - b)
   .reduce((a, b) => a + b, 0);
 
 console.log(combined);
-
 
 // Array Methods
 
